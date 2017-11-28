4033a0a59ef58d1a4f8a0cc3338a89e621f64680
Matt Carson <matthew.carson@northwestern.edu>
2017-11-28 12:18:47 -0600

    update after FY2018 Program Review

--- reshape_pub_source_ids.r
+++ reshape_pub_source_ids.r
@@ -1,7 +1,7 @@
 #### name: reshape_pub_source_ids.r
 ###  author: Matt Carson
 ##   creation date: 2017-6-13
-#    last update: 2017-9-14
+#    last update: 2017-11-28
 # 
 # This script is for reshaping a list of Publication IDs and associated Proprietary IDs from literature databases from:
 # 
@@ -25,24 +25,50 @@
 # .          .               .                .
 # 67890      <...>           <...>            <...>
 
+
+#!/usr/bin/env Rscript
+
+
+# If you want to run on the command line...
+#
+# Example: Rscript reshape_pub_source_ids.r -d "/My/Working/Directory" -i "input_file.csv" -o "output_file.csv"
+#
+require(docopt)
+
+'Usage:
+   reshape_pub_source_ids.r [-d <working directory> -i <input file> -o <output file>]
+
+Options:
+   -d Working directory
+   -i Input file name
+   -o Output file [default: reshaped_file.csv]
+
+ ]' -> doc
+
+opts <- docopt(doc)
+
+WORKING_DIR <- opts$d
+INPUT_FILE <- opts$i
+OUTPUT_FILE <- opts$o
+
+# If you want to run in RStudio...
+#WORKING_DIR = "dir_name"
+#INPUT_FILE = "input.csv"
+#OUTPUT_FILE = "reshaped_file.csv"
+
 # Load libraries
 library(reshape2)
 library(dplyr) # just for glimpse
 
-# Define file and directory names
-WORKING_DIR = "dir_name"
-INPUT_FILE = "input.csv"
-OUTPUT_FILE = "reshaped_file.csv"
-
 setwd(WORKING_DIR)
 
-# Import .csv file generated from Elements reporting database
+# Import .csv file from Elements reporting database
 publications <- read.csv(INPUT_FILE, header=TRUE)
 
 # Change ID to factor for summary
 publications$Publication.ID <- as.factor(publications$Publication.ID)
-                  
-# Examine and summarize the data frame using one or more of these:                 
+
+# Examine and summarize the data frame                    
 class(publications)
 dim(publications)
 names(publications)
@@ -51,13 +77,24 @@ glimpse(publications)
 summary(publications)
 
 # Create a df for reshaping
-long_df <- data.frame(publications$Publication.ID,publications$Data.Source.Proprietary.ID, publications$Data.Source)
+long_df <- data.frame("Group_Name" = publications$Group.Name,
+	                  "Last_Name" = publications$Last.Name,
+	                  "First_Name" = publications$First.Name,
+	                  "Department" = publications$Department,
+	                  "NetID" = publications$NetID,
+	                  "Employee_ID" = publications$Employee_ID,
+	                  "Scopus_AU-ID" = publications$Scopus.AU.ID,
+	                  "Pub_ID" = publications$Publication.ID,
+	                  "Data_Source_Proprietary_ID" = publications$Data.Source.Proprietary.ID,
+	                  "Data_Source" = publications$Data.Source)
 
 # Remove duplicate records
 long_df_dedup <- unique(long_df)
 
-# Reshape the long data frame to create a column for each Proprietary.ID type
-wide_df_dedup <- reshape(long_df_dedup, direction = "wide", idvar = "publications.Publication.ID", timevar= "publications.Data.Source", v.names = "publications.Data.Source.Proprietary.ID")
+# Reshape the long data frame to create a column for each "Data_Source_Proprietary_ID" type
+# By setting idvar = c("NetID", "Pub_ID") instead of just "Pub_ID", reshape doesn't deduplicate pubs. It will include each researchers full list of pubs,
+# even if multiple researchers in the file are authors of the same pub.
+wide_df_dedup <- reshape(long_df_dedup, direction = "wide", idvar = c("NetID", "Pub_ID"), timevar= "Data_Source", v.names = "Data_Source_Proprietary_ID")
 
 # If there are any Publication IDs with more than one instance of the same source ID (e.g., more than one Scopus or Web of Science ID),
 # the reshape command above will send a warning like this...
@@ -83,11 +120,10 @@ long_df_scopus_dup_count_sorted <- long_df_scopus_dup_count[with(long_df_scopus_
 # List of records with more than one Scopus ID
 long_df_scopus_dup_IDs <- long_df_scopus[long_df_scopus$publications.Publication.ID %in% long_df_scopus_dup_count$Var1[long_df_scopus_dup_count$Freq > 1],]
 
-
 # Rename the columns for readability
 #
 # Make sure the column order is correct before running
-names(wide_df_dedup) = c("Pub_ID", "PubMed", "Europe PubMed Central", "Scopus",	"Web of Science", "Crossref", "Web of Science (Lite)", "SSRN", "RePEc", "DBLP")
+names(wide_df_dedup) = c("Group_Name", "Last_Name", "First_Name", "Department", "NetID", "Employee_ID", "Scopus_AU-ID", "Pub_ID", "Europe PubMed Central", "Crossref", "PubMed", "Scopus", "Web of Science", "Web of Science (Lite)")
 
 # Export reshaped file as .csv
 write.csv(wide_df_dedup, file = OUTPUT_FILE, row.names = FALSE)
