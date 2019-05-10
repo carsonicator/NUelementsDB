f8a750d3c2aa57de8579e8e31d3188447ce0001d
carsonicator <kaarsinogen@gmail.com>
2019-05-09 23:27:38 -0500

    add new queries

--- SQL_Queries/query_collection.sql
+++ SQL_Queries/query_collection.sql
@@ -1,6 +1,78 @@
--- An example SQL query to generate a data set for reshape_pub_source_ids.r
--- All Cancer Center pubs from 2012 - present
 
+-- How do I get a list of all member of a group?
+use [Elements-reporting2]
+
+SELECT *
+FROM dbo.[Group User Membership] as gu
+JOIN dbo.[Group] as g
+	on g.[ID] = gu.[Group ID]
+WHERE  g.Name= 'group_name'
+
+
+-- How do I get a list of all member of a group with the user's personal information?
+use [Elements-reporting2]
+
+SELECT  u.[Last Name],
+        u.[First Name],
+        u.[Department],
+        u.[Username] AS "NetID",
+        u.[Proprietary ID] AS "Employee_ID"
+FROM dbo.[Group User Membership] as gu
+JOIN dbo.[Group] as g
+	on g.[ID] = gu.[Group ID]
+join dbo.[User] as u
+	on u.[ID] = gu.[User ID]
+WHERE  g.Name= 'group_name'
+
+
+-- How do I find our if a paper with a specific DOI is in the database?
+use [Elements-reporting2]
+SELECT *
+FROM [dbo].[Publication Record] as pr
+WHERE pr.[doi] = 'doi_name_1' OR pr.[doi] = 'doi_name_2' OR ...
+
+
+-- How do I return a list of all users in a group that have a [Scopus, ORCID, WOS] author ID?
+use [Elements-reporting2]
+
+SELECT  DISTINCT g.[Name] AS "Group Name",
+        u.[Last Name],
+        u.[First Name],
+        u.[Department],
+        u.[Username] AS "NetID",
+        u.[Proprietary ID] AS "Employee_ID",
+        idsch.[Name] AS "Author_ID_Scheme",
+        uia.[Identifier Value] AS "Author_ID"
+
+FROM    [dbo].[Group] AS g
+        -- get Users who are members of each group
+        JOIN [dbo].[Group User Membership] AS gu
+            ON gu.[Group ID] = g.[ID]
+        JOIN [dbo].[Publication User Relationship] AS pur
+            ON pur.[User ID] = gu.[User ID]
+        -- get each user's HR data
+        JOIN [dbo].[User] AS u
+            ON u.[ID] = pur.[User ID]
+        -- get each user's registered identifier data
+        JOIN [dbo].[User Identifier Association] AS uia
+            ON uia.[User ID] = u.[ID]
+        JOIN [dbo].[Identifier Scheme] AS idsch
+            ON idsch.ID = uia.[Identifier Scheme ID]
+WHERE   -- restrict to the group(s) of interest
+        g.[name] = 'group_name' AND idsch.[Name] = 'scopus-author-id'
+ORDER BY
+        "Group Name",
+        "Last Name",
+        "First Name",
+        "Department",
+        "NetID",
+        "Employee_ID",
+        "Author_ID_Scheme",
+        "Author_ID"
+;
+
+
+-- How do I get all publications within a date range for all members of a group?
 use [Elements-reporting2]
 
 SELECT g.name, pr.[Publication ID], doi, [Data Source], [Data Source Proprietary ID]
@@ -8,13 +80,12 @@ FROM [dbo].[Publication Record] as pr
 join [dbo].[Publication User Relationship] as pu on pr.[Publication ID] = pu.[Publication ID]
 join [dbo].[Group User Membership] as gu on gu.[User ID] = pu.[User ID]
 join [dbo].[Group] as g on g.[ID] = gu.[Group ID]
-WHERE [publication-date] > 20120101 AND [publication-date] <= 20170612 AND g.name = 'RHLCCC1'
+WHERE [publication-date] > YYYYMMDD AND [publication-date] <= YYYYMMDD AND g.name = 'group_name'
 ORDER BY pr.[Publication ID]
 
-----------------------------
 
--- A similar query with author and publication information
--- NOTE: Pubs may have duplicate publication dates, DOIs, and proprietary IDs (Scopus, WOS, etc.)
+-- A similar query with additional author and publication information
+-- NOTE: Pubs may have duplicate publication dates, DOIs, and proprietary IDs (Scopus, ORCID, WOS, etc.)
 
 use [Elements-reporting2]
 
@@ -24,14 +95,14 @@ join [dbo].[Publication User Relationship] as pu on pr.[Publication ID] = pu.[Pu
 join [dbo].[Group User Membership] as gu on gu.[User ID] = pu.[User ID]
 join [dbo].[User] as u on u.[ID] = pu.[User ID]
 join [dbo].[Group] as g on g.[ID] = gu.[Group ID]
-WHERE [publication-date] > 20120101 AND [publication-date] <= 20170612 AND g.name = 'RHLCCC1'
+WHERE [publication-date] > YYYYMMDD AND [publication-date] <= YYYYMMDD AND g.name = 'group_name'
 ORDER BY u.[Last Name]
 
 ---------------------------
 
 -- Querying for authors pubs when authors are in at least one of three groups
 -- Also includes Scopus Author ID and Employee_ID
--- NOTE: Pubs may have duplicate publication dates, DOIs, and proprietary IDs (Scopus, WOS, etc.)
+-- NOTE: Pubs may have duplicate publication dates, DOIs, and proprietary IDs (Scopus, ORCID, WOS, etc.)
 
 use [Elements-reporting2]
 
@@ -43,9 +114,9 @@ join [dbo].[User] as u on u.[ID] = pu.[User ID]
 join [dbo].[User Identifier Association] as uia on uia.[User ID] = u.[ID]
 join [dbo].[Identifier Scheme] as idsch on idsch.ID = uia.[Identifier Scheme ID]
 join [dbo].[Group] as g on g.[ID] = gu.[Group ID]
-WHERE pr.[publication-date] > 20130101 AND pr.[publication-date] <= 20171231 AND g.name = 'FAM_COM_MED_FY2018'
-   OR pr.[publication-date] > 20130101 AND pr.[publication-date] <= 20171231 AND g.name = 'RADIOLOGY_FY2018'
-   OR pr.[publication-date] > 20130101 AND pr.[publication-date] <= 20171231 AND g.name = 'SURGERY_FY2018'
+WHERE pr.[publication-date] > YYYYMMDD AND pr.[publication-date] <= YYYYMMDD AND g.name = 'group_name_1'
+   OR pr.[publication-date] > YYYYMMDD AND pr.[publication-date] <= YYYYMMDD AND g.name = 'group_name_2'
+   OR pr.[publication-date] > YYYYMMDD AND pr.[publication-date] <= YYYYMMDD AND g.name = 'group_name_3'
 ORDER BY u.[Last Name]
 
 --------------------------
@@ -124,52 +195,9 @@ WHERE u.[Department] like 'Pediatrics%'
 GROUP BY u.[Last Name], u.[First Name], u.[Department], u.Username, u.[Proprietary ID], uia.[Identifier Value], pr.[Publication ID], pr.[publication-date], doi, pr.[Data Source], pr.[Data Source Proprietary ID]
 ORDER BY u.[Last Name]
 
--- Is a paper with this DOI in the database?
 
-use [Elements-reporting2]
-SELECT *
-FROM [dbo].[Publication Record] as pr
-WHERE pr.[doi] = 'doi_name_1' OR pr.[doi] = 'doi_name_2' OR ...
 
--- How do I return a list of all users in a group that have a [Scopus, ORCID, WOS] author ID?
 
-use [Elements-reporting2]
-
-SELECT  DISTINCT g.[Name] AS "Group Name",
-        u.[Last Name],
-        u.[First Name],
-        u.[Department],
-        u.[Username] AS "NetID",
-        u.[Proprietary ID] AS "Employee_ID",
-        idsch.[Name] AS "Author_ID_Scheme",
-        uia.[Identifier Value] AS "Author_ID"
-        
-FROM    [dbo].[Group] AS g
-        -- get Users who are members of each group
-        JOIN [dbo].[Group User Membership] AS gu
-            ON gu.[Group ID] = g.[ID]
-        JOIN [dbo].[Publication User Relationship] AS pur
-            ON pur.[User ID] = gu.[User ID]
-        -- get each user's HR data
-        JOIN [dbo].[User] AS u
-            ON u.[ID] = pur.[User ID]
-        -- get each user's registered identifier data
-        JOIN [dbo].[User Identifier Association] AS uia
-            ON uia.[User ID] = u.[ID]
-        JOIN [dbo].[Identifier Scheme] AS idsch
-            ON idsch.ID = uia.[Identifier Scheme ID]
-WHERE   -- restrict to the group(s) of interest
-        g.[name] = 'group_name' AND idsch.[Name] = 'scopus-author-id'
-ORDER BY
-        "Group Name",
-        "Last Name",
-        "First Name",
-        "Department",
-        "NetID",
-        "Employee_ID",
-        "Author_ID_Scheme",
-        "Author_ID"
-;
 
 
 -- Returns all publications within a date range for researchers in group 'group_name'. Further comments on the 'Reporting Date 1' field needed.
