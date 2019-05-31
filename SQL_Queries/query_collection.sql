91af06641657bad19d862aa9a9a523ed460b8d2d
carsonicator <mbcarson@gmail.com>
2019-05-31 13:03:35 -0500

    added user and position queries

--- SQL_Queries/query_collection.sql
+++ SQL_Queries/query_collection.sql
@@ -7,13 +7,13 @@ SELECT  u.[Last Name],
 		u.[Position]
 FROM dbo.[User] as u
 
--- How do I get a list of all employee position titles and the number of people who hold each, ordered from most to least? 
+-- How do I get a list of all employee position titles and the number of people who hold each, ordered from most to least?
 SELECT  u.[Last Name],
         u.[First Name],
         u.[Department],
         u.[Username] AS "NetID",
         u.[Proprietary ID] AS "Employee_ID",
-		u.[Position]
+				u.[Position]
 FROM dbo.[User] as u
 
 -- How do I get a list of all member of a group?
